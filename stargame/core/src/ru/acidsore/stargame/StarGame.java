ad77906c4e9c903ec652907c25497b23d7e175de
AcidSore <INFomicheva@yandex.ru>
2019-01-18 21:00:31 +0300

    hw2  передвижение по кнопкам клавиатуры движение по нажатию не совсем корректно отрабатывает

--- stargame/core/src/ru/acidsore/stargame/StarGame.java
+++ stargame/core/src/ru/acidsore/stargame/StarGame.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -14,6 +15,10 @@ public class StarGame extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("maxresdefault.jpg");
+		Vector2 v1 = new Vector2(1,3);
+		Vector2 v2 = new Vector2(0,-1);
+		v1.add(v2);
+		System.out.println("v1+v2 = "+ v1.x+ v1.y);
 	}
 
 	@Override
